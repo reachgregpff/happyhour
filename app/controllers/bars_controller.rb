e78c4cfd745d30821ebaf99dd736184e9efa4da2
IvanYapeter <ivanyapeter@gmail.com>
2015-11-24 15:07:58 +1100

    Bars Marker Works

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -2,11 +2,11 @@ class BarsController < ApplicationController
 
   def index
     @bars = Bar.all
-    address = params[:address]
-    postcode = params[:postcode]
-    @location = address
-    if address != "Nearby"
-    	@location = @location + " " + postcode
+    @address = params[:address]
+    @postcode = params[:postcode]
+    @location = @address
+    if @address != "Nearby"
+    	@location = @location + " " + @postcode
     end
     @day = params[:day]
   end
