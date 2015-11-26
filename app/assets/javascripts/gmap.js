ad01692f6f9e33e53abb6a21bc218eeafd56460a
reachgregpff <gregpffwdi2@gmail.com>
2015-11-26 15:08:56 +1100

    yellow marker for my position and a few changes in the number scroll bar for cocktails

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -91,7 +91,7 @@ function rad(x) {return x*Math.PI/180;}
 
 
   // Check loction
-  console.log("Input location: " + location);
+  // console.log("Input location: " + location);
   map = new google.maps.Map($('.map')[0], {
     center: {lat: -34.397, lng: 150.644},
     zoom: 15
@@ -101,17 +101,23 @@ function rad(x) {return x*Math.PI/180;}
   if ( location === "Nearby" ) {
   // Try HTML5 geolocation.
     if (navigator.geolocation) {
-      console.log("Input location in NEARBY: " + location + " HERE");
+      // console.log("Input location in NEARBY: " + location + " HERE");
       navigator.geolocation.getCurrentPosition(function(position) {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        var infoWindow = new google.maps.InfoWindow({map: map});
-        infoWindow.setPosition(pos);
-        infoWindow.setContent('Your location');
+        // var infoWindow = new google.maps.InfoWindow({map: map});
+        // infoWindow.setPosition(pos);
+        // infoWindow.setContent('Your location');
         map.setCenter(pos);
-        console.log("LatLng of Nearby: " + pos);
+        var marker = new google.maps.Marker({
+            map: map,
+            // scrollwheel: false,
+            position: pos,
+            icon: "/assets/marker-star-3.png"
+        });
+        // console.log("LatLng of Nearby: " + pos);
         myLat = position.coords.latitude;
         myLong = position.coords.longitude;
         showBars();
@@ -127,15 +133,16 @@ function rad(x) {return x*Math.PI/180;}
 
   } else { // IF SOMETHING ELSE BUT NEARBY
     geocoder.geocode( { 'address': location }, function(results, status) {
-      console.log("Input location: " + location +" NOT NEARBY!");
+      //console.log("Input location: " + location +" NOT NEARBY!");
       if ( status == google.maps.GeocoderStatus.OK ) {
         map.setCenter(results[0].geometry.location);
         var marker = new google.maps.Marker({
             map: map,
-            scrollwheel: false,
-            position: results[0].geometry.location
+            // scrollwheel: false,
+            position: results[0].geometry.location,
+            icon: "/assets/marker-star-3.png"
         });
-        console.log("Other than Nearby LatLng: " + results[0].geometry.location);
+        //console.log("Other than Nearby LatLng: " + results[0].geometry.location);
         myLat = results[0].geometry.location.lat();
         myLong = results[0].geometry.location.lng();
         showBars();
